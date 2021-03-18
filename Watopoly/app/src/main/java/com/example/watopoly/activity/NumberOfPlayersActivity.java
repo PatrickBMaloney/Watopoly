4a8d124601a0f8c86f6ee6b20e4f1c455249b1b1
Patrick Maloney <maloneyp1210@gmail.com>
2021-03-18 10:48:12 -0400

    Add activity for choosing num players

--- Watopoly/app/src/main/java/com/example/watopoly/activity/NumberOfPlayersActivity.java
+++ Watopoly/app/src/main/java/com/example/watopoly/activity/NumberOfPlayersActivity.java
@@ -1,6 +1,51 @@
 package com.example.watopoly.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NumberOfPlayersActivity extends AppCompatActivity {
+import com.example.watopoly.R;
+
+public class NumberOfPlayersActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_num_players);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button btnOnePlayer = (Button) findViewById(R.id.numPlayersButton1);
+        Button btnTwoPlayers = (Button) findViewById(R.id.numPlayersButton2);
+        Button btnThreePlayers = (Button) findViewById(R.id.numPlayersButton3);
+        Button btnFourPlayers = (Button) findViewById(R.id.numPlayersButton4);
+        btnOnePlayer.setOnClickListener(this);
+        btnTwoPlayers.setOnClickListener(this);
+        btnThreePlayers.setOnClickListener(this);
+        btnFourPlayers.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, EnterPlayerInfoActivity.class);
+
+        switch(view.getId()) {
+            case R.id.numPlayersButton1:
+                intent.putExtra("numberOfPlayers", 1);
+            case R.id.numPlayersButton2:
+                intent.putExtra("numberOfPlayers", 2);
+            case R.id.numPlayersButton3:
+                intent.putExtra("numberOfPlayers", 3);
+            case R.id.numPlayersButton4:
+                intent.putExtra("numberOfPlayers", 4);
+            default:
+        }
+        startActivity(intent);
+    }
 }
