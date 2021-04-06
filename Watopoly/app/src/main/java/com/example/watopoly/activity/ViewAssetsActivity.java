1efa2126c881042809f378bc016b48206adf4a7e
Disa Prajapati <d4prajap@uwaterloo.ca>
2021-04-06 01:17:28 -0400

    View My Assets - Properties Completed

--- Watopoly/app/src/main/java/com/example/watopoly/activity/ViewAssetsActivity.java
+++ Watopoly/app/src/main/java/com/example/watopoly/activity/ViewAssetsActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -23,6 +24,9 @@ public class ViewAssetsActivity extends AppCompatActivity {
 
     private Game gameState = Game.getInstance();
     private PlayerInfoHeaderFragment playerInfoHeaderFragment;
+    View largeProp;
+    View buttons;
+    private Property prev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,8 @@ public class ViewAssetsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
+        largeProp = (View) findViewById(R.id.propertyCardBuyFragmentAssets);
+        buttons = (View) findViewById(R.id.actionsLinearLayoutAssets);
         setButtons();
         linkView();
         Player myPlayer = gameState.getCurrentPlayer();
@@ -55,7 +61,6 @@ public class ViewAssetsActivity extends AppCompatActivity {
                 }
             }
         });
-
         allAssetsButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -76,8 +81,54 @@ public class ViewAssetsActivity extends AppCompatActivity {
 
     }
     private void linkView() {
-        FragmentManager fm = getSupportFragmentManager();
+        final FragmentManager fm = getSupportFragmentManager();
         playerInfoHeaderFragment = (PlayerInfoHeaderFragment) fm.findFragmentById(R.id.playerInfoHeaderFragmentAssets);
+        int size = gameState.getCurrentPlayer().getProperties().size(); //size of properties
+        //will start setting all properties
+        for(int i = 0; i < size; i++) {
+            String frag = "propertyCardBuyFragment" + String.valueOf(i);
+            int resourceViewID = getResources().getIdentifier(frag, "id", getPackageName());
+            PropertyFragment propertyFragment = (PropertyFragment) fm.findFragmentById(resourceViewID);
+            final Property property = gameState.getCurrentPlayer().getProperties().get(i); //get the current property
+            propertyFragment.setProperty(property);
+            View prop = findViewById(resourceViewID);
+            prop.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    //populate large version
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN: {
+                            if(prev == null) {
+                                prev = property; //set prev to current if null
+                            }
+                            if (largeProp.getVisibility() == View.VISIBLE && prev==property) {
+                                largeProp.setVisibility(View.GONE);
+                                buttons.setVisibility(View.GONE);
+                            } else {
+                                PropertyFragment propLarge = (PropertyFragment) fm.findFragmentById(R.id.propertyCardBuyFragmentAssets); //get large prop
+                                propLarge.setProperty(property);
+                                largeProp.setVisibility(View.VISIBLE);
+                                buttons.setVisibility(View.VISIBLE);
+                                prev = property;
+                            }
+                        }
+                        default:
+                    }
+                    return true;
+                }
+            });
+        }
+
+        //set View to none for rest
+        for(int j = size; j < 28; j++) {
+            String frag = "propertyCardBuyFragment" + String.valueOf(j);
+            int resourceViewID = getResources().getIdentifier(frag, "id", getPackageName());
+            View hideFrag = findViewById(resourceViewID);
+            hideFrag.setVisibility(View.GONE);
+        }
+        //hide large view
+        largeProp.setVisibility(View.GONE);
+        buttons.setVisibility(View.GONE);
     }
 
 }
