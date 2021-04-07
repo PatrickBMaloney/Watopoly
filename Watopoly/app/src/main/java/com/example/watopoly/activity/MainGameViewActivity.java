e84cdff46583e95618237db8e7766578d2fcbfc8
Disa Prajapati <d4prajap@uwaterloo.ca>
2021-04-07 02:18:52 -0400

    final changes - completed view assets

--- Watopoly/app/src/main/java/com/example/watopoly/activity/MainGameViewActivity.java
+++ Watopoly/app/src/main/java/com/example/watopoly/activity/MainGameViewActivity.java
@@ -91,6 +91,13 @@ public class MainGameViewActivity extends AppCompatActivity implements FragmentC
 
         //TODO: bind button to the activity
         Button viewAssetButton = findViewById(R.id.viewAssetButton);
+        viewAssetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainGameViewActivity.this, ViewAssetsActivity.class);
+                startActivity(intent);
+            }
+        });
         Button tradeButton = findViewById(R.id.tradeButton);
         Button mortgageButton = findViewById(R.id.mortgageButton);
         Button endTurnButton = findViewById(R.id.endTurnButton);
