c3b58ab21b74931e1a70ccf152d3a3dc4c68a9b3
Michael Chau <michaelchau9817@gmail.com>
2021-04-09 01:01:13 -0400

    update mortgage property cells

--- Watopoly/app/src/main/java/com/example/watopoly/adapter/CellPropertyListAdapter.java
+++ Watopoly/app/src/main/java/com/example/watopoly/adapter/CellPropertyListAdapter.java
@@ -15,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.watopoly.R;
 import com.example.watopoly.model.Building;
 import com.example.watopoly.model.Property;
+import com.example.watopoly.model.Railway;
+import com.example.watopoly.model.Utility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +26,7 @@ import java.util.List;
 public class CellPropertyListAdapter extends RecyclerView.Adapter<CellPropertyListAdapter.PropertyInfoHolder> {
     private ArrayList<Property> properties;
     private List<Boolean> selected;
+    private Boolean singleSelect = false;
 
     public class PropertyInfoHolder extends RecyclerView.ViewHolder {
         ImageView propertyImageView;
@@ -31,6 +34,7 @@ public class CellPropertyListAdapter extends RecyclerView.Adapter<CellPropertyLi
         TextView propertyNameTextView;
         TextView unSelectTextView;
         TextView selectedTextView;
+        TextView amountTextView;
         PropertyInfoHolder (final View itemView) {
             super(itemView);
             propertyImageView = itemView.findViewById(R.id.cellPropertyImageView);
@@ -38,10 +42,12 @@ public class CellPropertyListAdapter extends RecyclerView.Adapter<CellPropertyLi
             propertyNameTextView = itemView.findViewById(R.id.cellPropertyNameTextView);
             unSelectTextView = itemView.findViewById(R.id.cardBorder);
             selectedTextView = itemView.findViewById(R.id.selectedBorder);
+            amountTextView = itemView.findViewById(R.id.cellPropertyAmountTextView);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //TODO: single select
                     int pos = getAdapterPosition();
                     selected.set(pos, selected.get(pos) ? false : true);
                     notifyDataSetChanged();
@@ -79,15 +85,19 @@ public class CellPropertyListAdapter extends RecyclerView.Adapter<CellPropertyLi
             holder.buildingNameTextView.setVisibility(View.GONE);
             holder.propertyImageView.setVisibility(View.VISIBLE);
             holder.propertyNameTextView.setVisibility(View.VISIBLE);
+
+            holder.propertyNameTextView.setText(property.getName());
+            if (property instanceof Utility) {
+                holder.propertyImageView.setImageResource(R.drawable.coffee_and_donut);
+            }else {
+                Railway railway = (Railway) property;
+                holder.propertyImageView.setImageResource(railway.getIcon());
+            }
         }
 
         boolean isSelected = selected.get(position);
         holder.selectedTextView.setVisibility(isSelected ? View.VISIBLE : View.GONE);
         holder.unSelectTextView.setVisibility(isSelected ? View.GONE : View.VISIBLE);
-
-        //TODO: for the others
-
-        //TODO: onSelected
     }
 
     @Override
@@ -98,6 +108,10 @@ public class CellPropertyListAdapter extends RecyclerView.Adapter<CellPropertyLi
     @Override
     public int getItemCount() {
         return properties.size();
+    }
+
+    public List<Boolean> getSelected() {
+        return selected;
     }
 }
 
